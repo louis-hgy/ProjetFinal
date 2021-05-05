054361075cfcbb53e1287330dc27bc68634a9c1c
CharlesS <charles.stchepinsky@gmail.com>
2021-05-05 18:13:38 +0200

    main fonction

--- Projet_NSI.py
+++ Projet_NSI.py
@@ -2,40 +2,42 @@ joueur1=([[0 for x in range(10)]for x in range(10)])
 joueur2=([[0 for x in range(10)]for x in range(10)])
 joueur1Tir=([[0 for x in range(10)]for x in range(10)])
 joueur2Tir=([[0 for x in range(10)]for x in range(10)])
-bateaux={'porte-avion': 5, 'croiseur': 4, 'sous-marin': 3, 'sous-marin': 3, 'torpilleur': 2}
+bateaux1={'porte-avion': 5, 'croiseur': 4, 'sous-marin': 3, 'contre-torpilleur': 3, 'torpilleur': 2}
+bateaux2={'porte-avion': 5, 'croiseur': 4, 'sous-marin': 3, 'contre-torpilleur': 3, 'torpilleur': 2}
 
-bateaux={'porte-avion': 5}
+bateaux2={'porte-avion': 5}
 
 def initialisation (joueur):
     
-    for cle,valeur in bateaux.items():
-        bateaux1 = list(input("Entrez la première extrémité du {} ({} cases)".format(cle, valeur)))
-        bateaux2 = list(input("Entrez la dernière extrémité du {} ({} cases)".format(cle, valeur)))
-        if bateaux1[0]==bateaux2[0]:
-            for i in range(int(bateaux1[1])-1,int(bateaux2[1])):
-                joueur[ord(bateaux1[0])-65][i]=cle
+    for cle,valeur in bateaux2.items():
+        bateau1 = list(input("Entrez la première extrémité du {} ({} cases)".format(cle, valeur)))
+        bateau2 = list(input("Entrez la dernière extrémité du {} ({} cases)".format(cle, valeur)))
+        if bateau1[0]==bateau2[0]:
+            for i in range(int(bateau1[1])-1,int(bateau2[1])):
+                joueur[ord(bateau1[0])-65][i]=cle
         else:
-            for i in range(ord(bateaux1[0])-65, ord(bateaux2[0])-64):
-                joueur[i][int(bateaux1[1])-1]=cle
+            for i in range(ord(bateau1[0])-65, ord(bateau2[0])-64):
+                joueur[i][int(bateau1[1])-1]=cle
                 
     
 #initialisation()
 #print(joueur1)
 #joueur1[0][0]='sous-marin'
 #joueur1[1][1]='torpilleur'
-def tir(joueur, case, bataille):
+def tir(joueurTir, case, bataille):
     if bataille!=None:
-        joueur[ord(case[0])-65][int(case[1])]=2
+        joueurTir[ord(case[0])-65][int(case[1])]=2
     else:
-        joueur[ord(case[0])-65][int(case[1])]=1
+        joueurTir[ord(case[0])-65][int(case[1])]=1
         
 
 
 
-def bataille(joueur, case):
+def bataille(joueur, bateaux, case):
     list(case)
     if joueur[ord(case[0])-65][int(case[1])] != 0 :
         if bateaux[joueur[ord(case[0])-65][int(case[1])]]-1 == 0:
+            bateaux[joueur[ord(case[0])-65][int(case[1])]]=0
             return 'Coulé'
         else:
             bateaux[joueur[ord(case[0])-65][int(case[1])]] -= 1
@@ -43,23 +45,35 @@ def bataille(joueur, case):
     else:
         return None
 
-    
+def perdu():
+    if bateaux1['porte-avion']==0:
+    #and bateaux1['croiseur']==0 and bateaux1['sous-marin']==0 and bateaux1['contre-torpilleur']==0 and bateaux1['torpilleur']==0:
+        return 'joueur1'
+    elif bateaux2['porte-avion']==0 :
+    #and bateaux2['croiseur']==0 and bateaux2['sous-marin']==0 and bateaux2['contre-torpilleur']==0 and bateaux2['torpilleur']==0:
+        return 'joueur2'
+    else:
+        return False
+        
 #bataille(joueur1, 'A1')
 #bataille(joueur1, 'B2')
 #bataille(joueur1, 'C3')
         
 initialisation(joueur1)
 initialisation(joueur2)
-navale=""
-while navale!=None:
-    case = list(input("Joueur 1, entrez une case"))
-    navale=bataille(joueur2, case)
-    print(navale)
-    tir(joueur1, case, navale)
-    print(joueur2)
-    print(joueur1Tir)
-navale=""
-while navale!=None:
-    case = list(input("Joueur 2, entrez une case"))
-    navale=bataille(joueur1, case)
-    tir(joueur2, case, navale)
+
+while perdu()==False:
+    navale=""
+    print(perdu())
+    while navale!=None and perdu()==False:
+        case = list(input("Joueur 1, entrez une case"))
+        navale=bataille(joueur2, bateaux2, case)
+        print(navale)
+        print( bateaux2['porte-avion'])
+        tir(joueur1Tir, case, navale)
+        
+    navale=""
+    while navale!=None and perdu()==False:
+        case = list(input("Joueur 2, entrez une case"))
+        navale=bataille(joueur1, bateaux1, case)
+        tir(joueur2Tir, case, navale)
